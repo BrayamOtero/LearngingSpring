45a194da0c4c5e7e37614a7eb3f89f017ea03405
Brayam Otero <davidotero@unicauca.edu.co>
2022-02-13 21:40:55 -0500

    Share information with Spring MVC and thymeleaf

--- src/main/java/com/example/holaspring/ControladorIncio.java
+++ src/main/java/com/example/holaspring/ControladorIncio.java
@@ -1,13 +1,23 @@
 package com.example.holaspring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ControladorIncio {
+
+    //Toma el valor definido en application.properties con la llave index.saludo
+    @Value("${index.saludo}")
+    private String saludo;
+
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        String mensaje = "Hola mundo con Thymeleaf";
+        model   .addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         return "index";
     }
 }
