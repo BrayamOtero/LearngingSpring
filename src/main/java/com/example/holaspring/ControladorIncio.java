8159c3d076615d33a197156266155582f4e25438
Brayam Otero <davidotero@unicauca.edu.co>
2022-02-14 01:14:24 -0500

    Create table with thymeleaf

--- src/main/java/com/example/holaspring/ControladorIncio.java
+++ src/main/java/com/example/holaspring/ControladorIncio.java
@@ -7,6 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class ControladorIncio {
 
@@ -17,16 +21,26 @@ public class ControladorIncio {
     @GetMapping("/")
     public String inicio(Model model){
         String mensaje = "Hola mundo con Thymeleaf";
-        Persona persona = new Persona();
 
+        Persona persona = new Persona();
         persona.setNombre("Brayam");
         persona.setApellido("PErez");
         persona.setEmail("dfgf");
         persona.setTelefono("12457");
+
+        Persona persona2 = new Persona();
+        persona2.setNombre("Carla");
+        persona2.setApellido("Gomezx");
+        persona2.setEmail("uykkiu");
+        persona2.setTelefono("096545");
+
+        List<Persona> personas = Arrays.asList(persona, persona2);
+
         model   .addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
 
-        model.addAttribute("persona", persona);
+//        model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 }
