6b0986b590686eec81c6a305206c977eae57be53
Brayam Otero <davidotero@unicauca.edu.co>
2022-02-13 21:22:00 -0500

    HolaMundo with thymeleaf

--- src/main/java/com/example/holaspring/ControladorIncio.java
+++ src/main/java/com/example/holaspring/ControladorIncio.java
@@ -1,12 +1,13 @@
 package com.example.holaspring;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ControladorIncio {
     @GetMapping("/")
     public String inicio(){
-        return "Hola Mundo con Spring 2";
+        return "index";
     }
 }
